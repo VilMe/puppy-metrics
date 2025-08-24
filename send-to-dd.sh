ae893f02551425307a9d2afc459ea0770dc6f7f7
VilMe <vilma.medrano00@gmail.com>
2025-08-24 17:55:16 -0500

    add curl command to submit test metric to DD account

--- send-to-dd.sh
+++ send-to-dd.sh
@@ -4,6 +4,27 @@
 
 echo "Good Day! How much does little Goose weigh today?" 
 read puppyweight
-date +%s 
+TIMENOW = date +%s 
 
 echo "Nice Goose weighs $puppyweight lbs! He's growing up so fast"
+
+curl -X POST "https://api.datadoghq.com/api/v2/series" \
+-H "Accept: application/json" \
+-H "Content-Type: application/json" \
+-H "DD-API-KEY: ${DD_API_KEY}" \
+-d @- << EOF
+{
+  "series": [
+    {
+      "metric": "testpuppyweight",
+      "type": 3,
+      "points": [
+        {
+          "timestamp": ${TIMENOW}
+          "value": ${puppyweight}
+        }
+      ],
+    }
+  ]
+}
+EOF
