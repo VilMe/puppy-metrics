110ba43bd3efa13fcc47a12321ddb2289cb862ac
VilMe <vilma.medrano00@gmail.com>
2025-08-24 14:19:44 -0500

    add some comments to direct and remind me of how to submit the metrics

--- send-to-dd.sh
+++ send-to-dd.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh 
 # enter puppyweight in terminal 
+# using the Datadog API https://docs.datadoghq.com/api/latest/metrics/#submit-metrics 
+
 echo "Good Day! How much does little Goose weigh today?" 
 read puppyweight
 date +%s 
