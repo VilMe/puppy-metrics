fa611565562f1fc459a2d44989b60004dde4d2cb
VilMe <vilma.medrano00@gmail.com>
2025-08-25 08:50:15 -0500

    add tags dog_name and part_of_day morning or night

--- send-to-dd.sh
+++ send-to-dd.sh
@@ -3,27 +3,32 @@
 # using the Datadog API https://docs.datadoghq.com/api/latest/metrics/#submit-metrics 
 
 echo "Good Day! How much does little Goose weigh today?" 
-read puppyweight
-TIMENOW = date +%s 
+read PUPPYWEIGHT
+TIMENOW=`date +%s` 
+echo "What time of day is it? (morning or night)"
+read TIMEOFDAY
 
-echo "Nice Goose weighs $puppyweight lbs! He's growing up so fast"
+echo "Nice Goose weighs $PUPPYWEIGHT lbs! He's growing up so fast $TIMENOW"
 
 curl -X POST "https://api.datadoghq.com/api/v2/series" \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
--H "DD-API-KEY: ${DD_API_KEY}" \
+-H "DD-API-KEY: $DD_API_KEY" \
 -d @- << EOF
 {
   "series": [
     {
       "metric": "testpuppyweight",
-      "type": 3,
+      "type": 0,
       "points": [
         {
-          "timestamp": ${TIMENOW}
-          "value": ${puppyweight}
+          "timestamp": $TIMENOW,
+          "value": $PUPPYWEIGHT
         }
       ],
+      "tags": [
+        "dog_name:Goose",
+        "part_of_day:$TIMEOFDAY"]
     }
   ]
 }
